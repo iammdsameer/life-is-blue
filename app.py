e0ca857fd42f81702a3f7a4a82145419a7f1e936
Sameer Khan <itsmdsameerkhan@gmail.com>
2020-11-20 08:18:40 +0545

    wip: needs enhancement, but does the basic task

---
+++ app.py
@@ -0,0 +1,26 @@
+import youtube_dl
+from flask import Flask, request, send_file
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+  return None
+
+@app.route('/tech/<url>')
+def home(url):
+  title = request.args.get('title')
+  filename = f'{title}.mp3'
+  ydl_opts = {
+      'format': 'bestaudio/best',
+      'postprocessors': [{
+          'key': 'FFmpegExtractAudio',
+          'preferredcodec': 'mp3',
+          'preferredquality': '192',
+      }],
+      'outtmpl': filename
+  }
+
+  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+      # info = ydl.extract_info(url, download=False)
+      ydl.download([f'https://youtu.be/{url}'])
+  return send_file(filename)
